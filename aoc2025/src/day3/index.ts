4b898f6a5f3887bb8eca6ea20bfc8ef0a323e389
Mikael Carlsson <mickec79@gmail.com>
2025-12-06 16:16:29 +0100

    part 2

--- aoc2025/src/day3/index.ts
+++ aoc2025/src/day3/index.ts
@@ -2,40 +2,33 @@
 
 import { fetchText } from '../utils/fetchText'
 
-function findMax(numbers: number[]) {
-  return numbers.reduce((a, b) => Math.max(a, b), -Infinity)
-}
+function getBatteries(data: string, length: number) {
+  const bank = data.split('').map(Number)
+  const batteries: number[] = []
+  let start = 0
 
-function getMaxJoltage(numbers: number[]) {
-  let n1: number | undefined
-  let n2: number | undefined
+  Array.from({ length }, (_, i) => {
+    const remaining = length - i
+    const end = bank.length - remaining + 1
+    const value = Math.max(...bank.slice(start, end))
+    start = bank.indexOf(value, start) + 1
+    batteries.push(value)
+  })
 
-  const max = findMax(numbers)
-  const index = numbers.indexOf(max)
-  const lastIndex = numbers.length - 1
-
-  if (index === lastIndex) {
-    n1 = findMax(numbers.slice(0, -1))
-    n2 = max
-  } else {
-    n1 = max
-    n2 = findMax(numbers.slice(index + 1))
-  }
-
-  return [n1, n2].join('')
+  return batteries.join('')
 }
 
 export default async function () {
-  const batteries = await fetchText('/src/day3/input.txt')
+  const banks = await fetchText('/src/day3/input.txt')
 
-  const maxJoltages = batteries.map((sequence, i) => {
-    const max = getMaxJoltage(sequence.split('').map(Number))
-    console.log(`${i + 1}) In "${sequence}" the largest joltage is ${max}`)
-    return max
+  const joltages = banks.map((bank, i) => {
+    const value = getBatteries(bank, 12)
+    console.log(`${i + 1}) In "${bank}" the largest joltage is ${value}`)
+    return value
   })
 
-  console.log(maxJoltages)
+  console.log(joltages)
 
-  const sum = maxJoltages.reduce((a, b) => a + Number(b), 0)
+  const sum = joltages.reduce((a, b) => a + Number(b), 0)
   console.log(`=== Total output joltage: ${sum} ===`)
 }
