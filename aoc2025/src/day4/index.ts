250815bd1b33f0aa8ac1d2064a15d460f148fc4b
Mikael Carlsson <mickec79@gmail.com>
2025-12-08 16:54:37 +0100

    part 2

--- aoc2025/src/day4/index.ts
+++ aoc2025/src/day4/index.ts
@@ -4,47 +4,63 @@ import { fetchText } from '../utils/fetchText'
 
 const isRoll = (input?: string) => input === '@' || input === 'x'
 
+const strMatch = (input: string, char: string) =>
+  input.match(new RegExp(char, 'g'))?.length ?? 0
+
 const rollCount = (input: string[]) =>
   input.reduce((sum, t) => sum + (isRoll(t) ? 1 : 0), 0)
 
-const range = (length: number, start: number = 0) => {
-  return Array.from({ length }, (_, i) => start + i)
-}
+const xCount = (input: string[]) =>
+  input.reduce((sum, s) => sum + strMatch(s, 'x'), 0)
 
 const MAGIC_NUMBER = 4
 const isAccessible = (input: string[]) => rollCount(input) < MAGIC_NUMBER
 
-export default async function () {
-  const data = await fetchText('src/day4/input.txt')
-  const results: string[][] = []
-
-  let accessibleRolls = 0
-
-  data.filter(Boolean).map((row, rowIndex, rows) => {
+function markAccessibleRolls(data: string[]) {
+  return data.map((row, rowIndex, rows) => {
     const prev = rows[rowIndex - 1]?.split('') ?? []
     const next = rows[rowIndex + 1]?.split('') ?? []
-
-    const result = row.split('').map((col, colIndex, cols) => {
-      const adjacentGrid = [
-        range(3, colIndex - 1).map((i) => prev[i] ?? ' '),
-        range(3, colIndex - 1).map((i) =>
-          i === colIndex ? '?' : cols[i] ?? ' '
-        ),
-        range(3, colIndex - 1).map((i) => next[i] ?? ' '),
-      ]
-
-      if (isRoll(col) && isAccessible(adjacentGrid.flat())) {
-        accessibleRolls++
-        return 'x'
-      }
-
-      return col
-    })
-
-    results.push(result)
+    return row
+      .split('')
+      .map((column, colIndex, cols) => {
+        const val = (r: string[], n: number) => r[colIndex + n] ?? ' '
+        const grid = [
+          [val(prev, -1), val(prev, 0), val(prev, 1)],
+          [val(cols, -1), /**/ '?' /**/, val(cols, 1)],
+          [val(next, -1), val(next, 0), val(next, 1)],
+        ]
+        if (isRoll(column) && isAccessible(grid.flat())) {
+          return 'x'
+        }
+        return column
+      })
+      .join('')
   })
+}
 
-  console.log(results)
+function deleteMarkedRolls(data: string[]) {
+  return data.map((row) => row.replaceAll('x', '.'))
+}
 
-  console.log(`=== Accessible rolls: ${accessibleRolls} ===`)
+export default async function () {
+  const input = await fetchText('src/day4/input.txt')
+
+  let removeCount = 0
+
+  console.log('Initial state', input)
+
+  function getResults(data: string[]) {
+    const marked = markAccessibleRolls(data)
+    const count = xCount(marked)
+    if (count > 0) {
+      console.log(`Remove ${count} rolls of paper:`)
+      console.log(marked)
+      removeCount += count
+      return getResults(deleteMarkedRolls(marked))
+    } else {
+      console.log(`== Removed ${removeCount} rolls of paper`)
+    }
+  }
+
+  getResults(input)
 }
